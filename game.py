e6fe6a45e51443cd268e9c04cc58026d0020bb85
Evan-Swihart <swidude@gmail.com>
2023-09-01 12:04:49 -0700

    Added restart, can be dead
    Added a restart function when game over. Made universal variable for speed and start/end locations, will have buttons to change in-game in future.

--- game.py
+++ game.py
@@ -25,19 +25,19 @@ def main():
     pygame.display.flip()
 
     # Set direction variables for snake
-    snake_x = 0
-    snake_y = 0
+    snake_start = 50
+    snake_end = 450
+    snake_x = snake_start
+    snake_y = snake_start
+    snake_speed = 0.05
     sx_change = 0
-    sy_change = 0.05
+    sy_change = snake_speed
 
     # Create green square to represent snake
     snake = pygame.Surface((snake_x, snake_y))
     snake = snake.convert()
-    snake.fill((0, 255, 0))
-    snake = pygame.draw.rect(screen, (0, 255, 0), (snake_x, snake_y, 10, 10))
-
-    # Draw snake
-    # screen.blit(snake, (snake_x, snake_y))
+    snake_color = (0, 255, 0)
+    snake = pygame.draw.rect(screen, snake_color, (snake_x, snake_y, 10, 10))
 
     # Set location variables for food
     food_x = 0
@@ -45,41 +45,89 @@ def main():
 
     # define a variable to control the main loop
     running = True
-     
+
+    # Set variable to determine if snake is alive
+    live = True
+
     # main loop
     while running:
         # check if the snake has hit the edge of the screen
-        if snake_x > 500 or snake_x < 0:
-            # Show "Game Over" screen
-            print("Game Over")
-            running = False
-        if snake_y > 500 or snake_y < 0:
-            # Show "Game Over" screen
-            print("Game Over")
-            running = False
+        if snake_x > 500 or snake_x < 0 or snake_y > 500 or snake_y < 0:
+            # GAME OVER CODE
+            # Set snake to dead
+            live = False
 
-        # change direction of snake based on keypress
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]:
+            # Set snake location to middle of screen (roughly)
+            snake_x = snake_end
+            snake_y = snake_end
+            # Set direction variables for snake
             sx_change = 0
-            sy_change = -0.05
-        elif pressed[pygame.K_DOWN]:
-            sx_change = 0
-            sy_change = 0.05
-        elif pressed[pygame.K_LEFT]:
-            sx_change = -0.05
             sy_change = 0
-        elif pressed[pygame.K_RIGHT]:
-            sx_change = 0.05
-            sy_change = 0
-            
-        # update snake location
-        snake_x += sx_change
-        snake_y += sy_change
+
+            # Set snake fill to black to hide it
+            snake_color = (0, 0, 0)
+
+            # Set text for "Game Over" screen
+            font = pygame.font.Font(None, 36)
+            text = font.render("Game Over", 1, (255, 255, 255))
+            textpos = text.get_rect()
+            textpos.centerx = background.get_rect().centerx
+            textpos.centery = background.get_rect().centery-15
+            background.blit(text, textpos)
+            # Set text for "Restart" screen
+            text = font.render("Press R to restart", 1, (255, 255, 255))
+            textpos = text.get_rect()
+            textpos.centerx = background.get_rect().centerx
+            textpos.centery = background.get_rect().centery + 15
+            background.blit(text, textpos)
+
+
+        # if snake is alive, play game
+        if live == True:
+            # change direction of snake based on keypress
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_UP] or pressed[pygame.K_w]:
+                sx_change = 0
+                sy_change = -snake_speed
+            elif pressed[pygame.K_DOWN] or pressed[pygame.K_s]:
+                sx_change = 0
+                sy_change = snake_speed
+            elif pressed[pygame.K_LEFT] or pressed[pygame.K_a]:
+                sx_change = -snake_speed
+                sy_change = 0
+            elif pressed[pygame.K_RIGHT] or pressed[pygame.K_d]:
+                sx_change = snake_speed
+                sy_change = 0
+
+            # update snake location
+            snake_x += sx_change
+            snake_y += sy_change
+
+        # if snake is dead, wait for keypress to restart game
+        if live == False:
+            # when key is pressed, reset game
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_r]:
+                # Set snake to alive
+                live = True
+
+                # Set snake location to almost top left
+                snake_x = snake_start
+                snake_y = snake_start
+
+                # Set direction variables for snake
+                sx_change = 0
+                sy_change = snake_speed
+
+                # Set snake fill to green
+                snake_color = (0, 255, 0)
+
+                # Hide "Game Over" and "Restart" text
+                background.fill((0, 0, 0))
 
         # blit everything to the screen
         screen.blit(background, (0, 0))
-        snake = pygame.draw.rect(screen, (0, 255, 0), (snake_x, snake_y, 10, 10))
+        snake = pygame.draw.rect(screen, snake_color, (snake_x, snake_y, 10, 10))
 
         # update screen drawing
         pygame.display.flip()
